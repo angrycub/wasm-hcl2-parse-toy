5e2f69bdeac11e3ff7c7e41d0f442f63834151d0
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2021-02-05 16:43:31 -0500

    Initial commit

---
+++ cmd/wasm/go.mod
@@ -0,0 +1,8 @@
+module github.com/angrycub/wasm-nomad-job-parser
+
+go 1.15
+
+require (
+	github.com/hashicorp/nomad v1.0.2
+	github.com/hashicorp/nomad/api v0.0.0-20210128000543-5bbe53f2228a
+)
---
+++ cmd/wasm/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (  
+    "fmt"
+    "strings"
+    "encoding/json"
+    "syscall/js"
+
+    "github.com/hashicorp/nomad/api"
+    "github.com/hashicorp/nomad/jobspec2"
+)
+
+func main() {  
+    fmt.Println("Go Web Assembly")
+    js.Global().Set("formatJSON", jsonWrapper()) 
+    <-make(chan bool)
+}
+
+func prettyJson(input string) (string, error) {  
+    var raw interface{}
+    if err := json.Unmarshal([]byte(input), &raw); err != nil {
+        return "", err
+    }
+    pretty, err := json.MarshalIndent(raw, "", "  ")
+    if err != nil {
+        return "", err
+    }
+    return string(pretty), nil
+}
+
+func jsonWrapper() js.Func {  
+    jsonfunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+        if len(args) != 1 {
+            result := map[string]interface{}{
+                "error": "Invalid no of arguments passed",
+            }
+            return result
+        }
+        jsDoc := js.Global().Get("document")
+        if !jsDoc.Truthy() {
+            result := map[string]interface{}{
+                "error": "Unable to get document object",
+            }
+            return result
+        }
+        jsonOuputTextArea := jsDoc.Call("getElementById", "jsonoutput")
+        if !jsonOuputTextArea.Truthy() {
+            result := map[string]interface{}{
+                "error": "Unable to get output text area",
+            }
+            return result
+        }
+        inputJSON := args[0].String()
+        fmt.Printf("input %s\n", inputJSON)
+        // pretty, err := prettyJson(inputJSON)
+        pretty, err := parseHCL(inputJSON)
+        if err != nil {
+            errStr := fmt.Sprintf("unable to parse HCL. Error %s occurred\n", err)
+            result := map[string]interface{}{
+                "error": errStr,
+            }
+            return result
+        }
+        jsonOuputTextArea.Set("value", pretty)
+        return nil
+    })
+    return jsonfunc
+}
+
+
+func parseHCL(jobspec string) (string, error) {
+    // create a destination job. these default values should
+    // be stomped by the incoming ones.
+    myJob := api.NewServiceJob("test", "test", "global", 50)
+    myJob, err := jobspec2.Parse("", strings.NewReader(jobspec))
+    if err != nil {
+        return "", err
+    }
+    out, err := json.MarshalIndent(myJob, "", "  ")
+    if err != nil {
+        return "", err
+    }
+    return string(out), nil
+}
+
